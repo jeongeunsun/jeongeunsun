1652f813d39d111d770905c69a6caf5c539dfefe
DESKTOP-F4BC13L\hi <dmstjs915@gmail.com>
2024-03-20 10:04:57 +0900

    first commit

---
+++ testProject/src/test.java
@@ -0,0 +1,13 @@
+import java.sql.SQLOutput;
+
+public class test {
+
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        System.out.println("하이");
+        System.out.print("하이");
+        System.out.print("하이");
+        System.out.println("@_@");
+        System.out.print("하이");
+    }
+}
