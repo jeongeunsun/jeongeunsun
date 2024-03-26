9382e6163d25140900eb8c6633127405a318a686
DESKTOP-F4BC13L\hi <dmstjs915@gmail.com>
2024-03-26 16:54:31 +0900

    배열 관련 예시 커밋

---
+++ chap05-array-lecture-source/src/com/ohgiraffers/section02/demensinal/Application4.java
@@ -0,0 +1,19 @@
+package com.ohgiraffers.section02.demensinal;
+
+public class Application4 {
+    public static void main(String[] args) {
+        /* 수업목표. 2차원 배열의 선언과 할당 및 초기화를 동시에 할 수 있다. */
+        /* 2차원 배열도 할당과 동시에 기본값 외에 값으로 초기화 하고 싶은 경우 리터럴을 이요할 수 있다. */
+        /* 1. 선언과 할당을 하며 초기화 */
+        /* 1-1. 정변배열 */
+        int[][] iarr = {{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15}}; // int[3][5]와 같다
+
+        for(int i = 0; i < iarr.length; i++){
+            for(int j = 0; j < iarr[i].length; j++){
+                System.out.print(iarr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
