17c6a8e5bc010c441bbfc0ab7446fdf6f41a4660
DESKTOP-F4BC13L\hi <dmstjs915@gmail.com>
2024-03-28 11:15:38 +0900

    클래스, 캡슐화 커밋

---
+++ chap06-class-and-object-lecture-source/src/com/ohgiraffers/section01/user_type/Member.java
@@ -0,0 +1,12 @@
+package com.ohgiraffers.section01.user_type;
+
+public class Member {
+    //class니까 main 필요없음
+    //아래를 필드명을 선언한다 . 초기화는 하면 안됨
+    String id;
+    String pwd;
+    String name;
+    int age;
+    char gender;
+    String[] hobby;
+}
---
+++ chap06-class-and-object-lecture-source/src/com/ohgiraffers/section02/encapsulation/problem2/Application.java
@@ -0,0 +1,31 @@
+package com.ohgiraffers.section02.encapsulation.problem2;
+
+public class Application {
+    public static void main(String[] args) {
+        /* 필드에 직접 접근하는 경우 발생할 수 있는 문제점을 이해할 수 있다. */
+
+        /*
+        Monster moster1 = new Monster();
+        moster1.name = "피카츄";
+        moster1.hp = 100;
+
+        Monster moster2 = new Monster();
+        moster1.name = "파이리";
+        moster1.hp = 200;
+
+        Monster moster3 = new Monster();
+        moster1.name = "이상해씨";
+        moster1.hp = 300;
+
+        System.out.println("moster1.name = " + moster1.name);
+        System.out.println("moster1.hp = " + moster1.hp);
+        System.out.println("moster2.name = " + moster2.name);
+        System.out.println("moster2.hp = " + moster2.hp);
+        System.out.println("moster3.name = " + moster3.name);
+        System.out.println("moster3.hp = " + moster3.hp);
+        */      //필드명의 이름을 바꾸면 다 바꿔야한다는 불편함이 있다..
+
+
+
+    }
+}
