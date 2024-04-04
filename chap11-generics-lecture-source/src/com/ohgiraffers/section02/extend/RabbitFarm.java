23bd7c50e923866dc3a02b6bc50215c3925e24d1
DESKTOP-F4BC13L\hi <dmstjs915@gmail.com>
2024-04-04 12:41:08 +0900

    제네릭

---
+++ chap11-generics-lecture-source/src/com/ohgiraffers/section02/extend/RabbitFarm.java
@@ -0,0 +1,24 @@
+package com.ohgiraffers.section02.extend;
+
+//public class RabbitFarm<T implements Animal> {    //에러남
+//public class RabbitFarm<T extends Animal> { //정상
+
+public class RabbitFarm<T extends Rabbit> {
+    //public class RabbitFarm<T extends Rabbit & Animal>{ //클래스 & 인터페이스 상속
+    //public class RabbitFarm<T extends Rabbit, Snake>{
+
+    private T animal;
+
+    public RabbitFarm(){};
+    public RabbitFarm(T animal){
+        this.animal = animal;
+    }
+
+    public T getAnimal() {
+        return animal;
+    }
+
+    public void setAnimal(T animal) {
+        this.animal = animal;
+    }
+}
